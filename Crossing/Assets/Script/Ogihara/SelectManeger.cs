1908e13eb7e0d9f59f0f241fbd3a6b359ec41f9a
ogihararyuki <nkcgta.ogihara.ryuki@gmail.com>
2021-03-02 10:53:45 +0900

    シーン追加、スクリプト追加、画像追加

---
+++ Crossing/Assets/Script/Ogihara/SelectManeger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectManeger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnClickStart1()
+    {
+        SceneManager.LoadScene("1-1");               
+    }
+    public void OnClickStart2()
+    {
+        SceneManager.LoadScene("1-2");
+    }
+    public void OnClickStart3()
+    {
+        SceneManager.LoadScene("1-3");
+    }
+    public void OnClickStart4()
+    {
+        SceneManager.LoadScene("1-4");
+    }
+}
