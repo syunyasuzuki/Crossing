9bf76c3e4f92dce4728684d135814a7b37474da3
UmemuraYuto <polo0820yuto@yahoo.co.jp>
2021-03-08 15:17:01 +0900

    BGM追加、BGM逆再生追加

---
+++ Crossing/Assets/Script/Umemura/BGMcon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMcon : MonoBehaviour
+{
+    AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            audio.pitch = -3.0f;
+        }
+        else
+        {
+            audio.pitch = 1.0f;
+        }
+        
+    }
+}
